e0e66333a4c954a8c126a51f0a0f4172d70f7954
Aadhi <87893573+Aadhi000@users.noreply.github.com>
2022-01-20 23:39:34 +0530

    Update index.py

--- plugins/index.py
+++ plugins/index.py
@@ -30,7 +30,7 @@ async def index_files(bot, message):
                 await last_msg.reply_text(f"This Is An Invalid Message, Either the channel is private and bot is not an admin in the forwarded chat, or you forwarded message as copy.\nError caused Due to <code>{e}</code>")
                 continue
 
-        msg = await message.reply('Processing...⏳')
+        msg = await message.reply('𝐇𝐚𝐜𝐤𝐢𝐧𝐠 𝐀𝐥𝐥...🙈')
         total_files = 0
         async with lock:
             try:
