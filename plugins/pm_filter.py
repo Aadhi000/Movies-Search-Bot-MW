8caebaad87a105ae0c33f55f42e849d8fa5595ed
Aadhi <87893573+Aadhi000@users.noreply.github.com>
2022-01-22 17:32:22 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -59,7 +59,7 @@ async def filter(client, message):
                     [InlineKeyboardButton(text=f"{filename}",callback_data=f"subinps#{file_id}")]
                     )
         else:
-            await client.send_sticker(chat_id=message.from_user.id, sticker='AgADMwIAAtbcmFc')
+            await client.send_sticker(chat_id=message.from_user.id, sticker='2ff0afc54240cc5e089a3')
             return
 
         if not btn:
