fb193c29b1c12c706e97754e76d2a1173e03143e
Aadhi <87893573+Aadhi000@users.noreply.github.com>
2022-01-28 00:24:36 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -94,7 +94,7 @@ async def start(bot, cmd):
         )
     else:
         await cmd.reply_text(
-            elif query.data == "start":
+    elif query.data == "start":
             START_MSG,
             parse_mode="Markdown",
             disable_web_page_preview=True,
