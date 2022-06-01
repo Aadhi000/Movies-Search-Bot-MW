0bde3e5a954dbb6ef5119ee95660e8198c6d5e18
ααdhí | αrjun vr <87893573+Aadhi000@users.noreply.github.com>
2022-06-01 15:08:01 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -68,6 +68,27 @@ async def save_poster(imdb_id, title, year, url):
         else:
             logger.info("Poster is saved in database")
 
+async def broadcast_messages(user_id, message):
+    try:
+        await message.copy(chat_id=user_id)
+        return True, "Succes"
+    except FloodWait as e:
+        await asyncio.sleep(e.x)
+        return await broadcast_messages(user_id, message)
+    except InputUserDeactivated:
+        await db.delete_user(int(user_id))
+        logging.info(f"{user_id}-Removed from Database, since deleted account.")
+        return False, "Deleted"
+    except UserIsBlocked:
+        logging.info(f"{user_id} -Blocked the bot.")
+        return False, "Blocked"
+    except PeerIdInvalid:
+        await db.delete_user(int(user_id))
+        logging.info(f"{user_id} - PeerIdInvalid")
+        return False, "Error"
+    except Exception as e:
+        return False, "Error"
+
 async def save_file(media):
     """Save file in database"""
 
